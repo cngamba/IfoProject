2868da6db7bf043de1e1dd4cd76a8c8f27e52375
Chouhei Ngamba Ngangom <cngamba@Chouheis-MacBook-Pro.local>
2016-12-22 15:49:39 -0600

    Initial Commit

---
+++ IfoProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IfoProject
+//
+//  Created by Chouhei Ngamba Ngangom on 22/12/16.
+//  Copyright © 2016 cGenes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
